e94fcea63ba785f47799b2f487b6d166fbf86a2b
Cory Bateman <splintercode.cb@gmail.com>
2014-03-09 20:10:18 -0500

    New CoreSpeedTest
    New library update

---
+++ CoreSpeedTest.js
@@ -0,0 +1,69 @@
+﻿window.coreSpeedTest = (function () {
+    var startTime, endTime, _settings, _callback, _latencyTime = 0, _speedbps = 0, _speedKbps = 0, _speedMbps = 0;
+    var _downloadImage = 'imageLarge.jpg', _latencyImage = 'imageSmall.gif';
+
+    function getLatencySpeed(callback) {
+        var latencyDuration, latencyStartTime, latencyEndTime;
+        var imageAddr = _latencyImage + "?n=" + Math.random();
+        var latencyTestImage = new Image();
+
+        latencyStartTime = (new Date()).getTime();
+        latencyTestImage.src = imageAddr;
+        latencyTestImage.onload = function () {
+            latencyEndTime = (new Date()).getTime();
+            _latencyTime = (latencyEndTime - latencyStartTime) / 1000;
+            getDownloadSpeed();
+        }
+    }
+
+    function getDownloadSpeed() {
+        var imageAddr = _downloadImage + '?n=' + Math.random();
+        var download = new Image();
+
+        startTime = (new Date()).getTime();
+        download.src = imageAddr;
+        download.onload = function () {
+            endTime = (new Date()).getTime();
+            setDownloadSpeeds();
+        }
+    }
+
+    function setDownloadSpeeds() {
+        var downloadSize = [236197]; // Num of Bytes for large file
+        var duration = (endTime - startTime) / 1000;
+        var bitsLoaded = downloadSize * 8;
+
+        _speedBps = (bitsLoaded / duration).toFixed(3);
+        _speedKbps = (_speedBps / 1024).toFixed(3);
+        _speedMbps = (_speedKbps / 1024).toFixed(3);
+
+        if (_callback) {
+            _callback();
+        }
+    }
+
+    return {
+        settings: {
+            downloadImage: function (e) { if (e) { _downloadImage = e } },
+            latencyImage:  function (e) { if (e) { _latencyImage  = e } }
+        },
+
+        latencyTime: function () { return _latencyTime },
+        speedBps: function () { return _speedBps },
+        speedKbps: function () { return _speedKbps },
+        speedMbps: function () { return _speedMbps },
+
+        init: function () {
+            _settings = this.settings;
+        },
+
+        test: function (callback) {
+            _settings = this.settings;
+            getLatencySpeed();
+            if (callback) {
+                _callback = callback;
+            }
+        }
+    };
+}());
+coreSpeedTest.init();
--- app.js
+++ app.js
@@ -1,60 +1,34 @@
-﻿app = angular.module('SpeedTest', ['ngSanitize']);
+﻿// coreSpeedTest.settings.latencyImage("one.gif");   // Set custom file to test latency speed
+// coreSpeedTest.settings.downloadImage("two.jpg");  // Set custom file to test download speed
+// coreSpeedTest.test();                             // Test on load of page
 
+app = angular.module('SpeedTest', ['ngSanitize']);
 app.controller('TestCtrl', function ($scope) {
 
     $scope.startLatency = 0;
-    $scope.downloadSpeed = '';
     $scope.speedBps = 0;
     $scope.speedKbps = 0;
     $scope.speedMbps = 0;
-
     $scope.buttonText = 'Test';
 
-    var startTime, endTime;
-
     $scope.startTest = function () {
-        getStartLatencySpeed(getDownloadSpeed);
+        $scope.buttonText = '<i class=\'icon-refresh icon-spin\'></i>';
+        coreSpeedTest.test(function () {
+            setSpeedValues();
+        });
     };
 
-    function getDownloadSpeed() {
-        var imageAddr = 'imageLarge.jpg' + '?n=' + Math.random();
-        var download = new Image();
-        download.src = imageAddr;
+    function setSpeedValues() {
+        $scope.buttonText = 'Test Complete';
+        $scope.startLatency = coreSpeedTest.latencyTime();
+        $scope.speedBps = coreSpeedTest.speedBps();
+        $scope.speedKbps = coreSpeedTest.speedKbps();
+        $scope.speedMbps = coreSpeedTest.speedMbps();
+        $scope.$apply();
 
-        startTime = (new Date()).getTime();
-        download.onload = function () {
-            endTime = (new Date()).getTime();
-            showDownloadResults();
-            $scope.buttonText = 'Test';
-            $scope.$apply();
-        }
-    }
-
-    function showDownloadResults() {
-        var downloadSize = [236197]; // Bytes for large file
-        var duration = (endTime - startTime) / 1000;
-        var bitsLoaded = downloadSize * 8;
-        $scope.speedBps = (bitsLoaded / duration).toFixed(3);
-        $scope.speedKbps = ($scope.speedBps / 1024).toFixed(3);
-        $scope.speedMbps = ($scope.speedKbps / 1024).toFixed(3);
-    }
-
-    function getStartLatencySpeed() {
-        $scope.buttonText = '<i class=\'icon-refresh icon-spin\'></i>';
-
-        var latencyDuration;
-        var startLatencyTime, endLatencyTime;
-
-        var imageAddr = "imageSmall.gif" + "?n=" + Math.random();
-        var downloadLatency = new Image();
-        downloadLatency.src = imageAddr;
-
-        startLatencyTime = (new Date()).getTime();
-        downloadLatency.onload = function () {
-            endLatencyTime = (new Date()).getTime();
-            latencyDuration = (endLatencyTime - startLatencyTime) / 1000;
-            $scope.startLatency = latencyDuration;
-            getDownloadSpeed();
-        }
+        console.log("Configuring Download Speeds");
+        console.log($scope.speedBps + " Bps");
+        console.log($scope.speedKbps + " Kbps");
+        console.log($scope.speedMbps + " Mbps");
     }
 });
